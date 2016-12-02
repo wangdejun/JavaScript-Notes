dff544b144d760ad03b85b1fa5cf2d46dd5c07f1
wangdejun <1074201799@qq.com>
2016-12-02 23:57:10 +0800

    string

--- Node/nodetest.js
+++ Node/nodetest.js
@@ -8,6 +8,18 @@ function(req,res){
 			return;
 		}
 	}
-	//handle 404 request
 	handle404(req,res);
 }
+
+// function(req,res){
+// 	var pathname = url.parse(req.url).pathname;
+// 	for(var i=0;i<routes.length;i++){
+// 		var routes = routes[i];
+// 		if(pathname===routes[0]){
+// 			var action = routes[1];
+// 			action(req,res);
+// 			return;
+// 		}
+// 	}
+// 	hanle404(req,res)
+// }

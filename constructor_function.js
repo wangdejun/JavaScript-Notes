656a62628614dfcd589eb6dca45b45ed673e2de5
wangdejun <1074201799@qq.com>
2016-12-05 23:48:40 +0800

    method of a object

--- constructor_function.js
+++ constructor_function.js
@@ -2,7 +2,12 @@ function person(firstname,lastname,age,eyecolor){
 	this.firstname = firstname;
 	this.lastname = lastname;
 	this.age = age;
-	this.eyecolor = eyecolor
+	this.eyecolor = eyecolor;
+
+	this.changeName = changeName;
+	function changeName(name){
+		this.lastname = name
+	}
 }
 
 var herFather = new person("John","Doe",50,"blue")
@@ -12,3 +17,7 @@ console.log(person)
 console.log(herMather)
 console.log(herFather)
 console.log(herFather.eyecolor)
+
+herMather.changeName("TOM")
+console.log("after her mother change name ")
+console.log(herMather)

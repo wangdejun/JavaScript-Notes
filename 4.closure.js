da9b2573467f8ef36342df8319070bb905b376c5
wangdejun <1074201799@qq.com>
2016-12-09 23:55:22 +0800

    add rem_inpractice.html

--- 4.closure.js
+++ 4.closure.js
@@ -9,16 +9,20 @@ var pet = function(name) {          //外部函数定义了一个变量"name"
 myPet = pet("Vivie");
 console.log(myPet()); 
 
-function settingCountry(country_param){
-    var getName = function(){
-        if(country_param){
-            return country_param
-        }
-    }
-    return getName;
-}
-myCountry=settingCountry("zh")
-console.log(myCountry());
-myCountry=settingCountry("cn")
-console.log(myCountry());
-console.log(myCountry());
+
+
+
+
+// function settingCountry(country_param){
+//     var getName = function(){
+//         if(country_param){
+//             return country_param
+//         }
+//     }
+//     return getName;
+// }
+// myCountry=settingCountry("zh")
+// console.log(myCountry());
+// myCountry=settingCountry("cn")
+// console.log(myCountry());
+// console.log(myCountry());

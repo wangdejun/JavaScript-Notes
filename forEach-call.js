56498f8d3ed007252fdb6165011669708723edf9
wangdejun <1074201799@qq.com>
2016-12-18 17:46:26 +0800

    some notes

---
+++ forEach-call.js
@@ -0,0 +1,12 @@
+function forEach(list,callback){
+    for(var n=0;n<list.length;n++){
+        callback.call(list[n],n)
+    }
+}
+
+var weapons = ["guns","canons","knife"]
+
+forEach(weapons,function(index){
+
+    console.log(index,weapons[index])
+})

bcec9e9ccefefd52c491b49c021022712c7758b7
wangdejun <1074201799@qq.com>
2016-12-19 09:36:46 +0800

    4 method to call a function

--- call&apply.js
+++ call&apply.js
@@ -1,4 +1,4 @@
-###调用函数的四种方式
+/**
  * 调用函数的方式有4种
  * 第一种：声明函数调用函数
  * 	作用域是全局
@@ -8,3 +8,20 @@
  *	作用域是返回的这个新的构建的对象
  * 第四种：call()和 apply()
  *	作用域可以由自己来指定，同时选用更好地传入参数列表的方法。
+ */
+
+function juggle(){
+	var result = 0;
+	for(var n=0;n<arguments.length;n++){
+		result += arguments[n];
+	}
+	this.result = result;
+}
+var ninja1 = {};
+var ninja2 = {};
+
+juggle.apply(ninja1,[1,2,3,4]);
+juggle.call(ninja2,5,6,7,8);
+
+console.log(ninja1.result)
+console.log(ninja2.result)

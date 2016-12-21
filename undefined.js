e7cb56f558dfbc9d70ebe55e75f3d139170f032c
wangdejun <1074201799@qq.com>
2016-12-21 23:56:49 +0800

    NaN

--- undefined.js
+++ undefined.js
@@ -5,7 +5,11 @@ console.log(typeof(a))
 //数组其实是个对象
 a = null;
 console.log(typeof(a))
-//null是未引用，也是一个对象
+//null是未引用，也是一个对象，是一个只有一个值的特殊类型。表示一个空对象引用。
+//表示什么都没有
 
 console.log(undefined==null)
 console.log(undefined===null)
+
+a = NaN
+console.log(typeof(a))

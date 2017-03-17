86ddea569e752807841bc5ae123bc79821dacfde
wangdejun <1074201799@qq.com>
2017-03-17 10:47:49 +0800

    rename

--- designpattern/1_1_add_methods_to_object_prototype.js
+++ designpattern/1_1_add_methods_to_object_prototype.js
@@ -1,12 +1,13 @@
-
 //用这个prototype扩展了原生Obj的方法
 Object.prototype.addMethod = function(name,fn){
 	this[name]=fn;
+	console.log(this);
 	return this
 };
 
 //new一个对象 
 var a = new Object();
+console.log(a);
 //给这个对象通过addMethod添加
 a.addMethod("checkEmail",function(){
 	console.log("I 'm checking checkEmail");
@@ -17,3 +18,5 @@ a.addMethod("checkEmail",function(){
 })
 
 a.checkEmail().checkPassword()
+
+console.log(a instanceof Object)
--- designpattern/2_6_ClassInherition_Class_via_constructor.js
+++ designpattern/2_6_ClassInherition_Class_via_constructor.js
@@ -14,7 +14,6 @@ function SubClass(id){
 //创建第一个 子类实例
 var instance_1 = new SubClass(10);
 
-
 var instance_2 = new SubClass(11);
 
 

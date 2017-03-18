badfbd7a221cebc7ad12295d957af168951a0d75
wangdejun <1074201799@qq.com>
2017-03-18 17:56:49 +0800

    Simple Factory Design pattern

--- designpattern/3_1SimpleFactory.js
+++ designpattern/3_1SimpleFactory.js
@@ -14,7 +14,6 @@ Basketball.prototype = {
 var Football = function(){
 	this.intro = "足球盛行于世界"
 }
-
 Football.prototype = {
 	getMember:function(){
 		console.log('足球运动需要11个人');
@@ -23,3 +22,75 @@ Football.prototype = {
 		console.log("足球很大");
 	}
 };
+
+var Tennis = function(){
+	this.intro = "每年有很多系列网球公开赛";
+}
+Tennis.prototype = {
+	getMember:function(){
+		console.log('网球运动需要1个人');
+	},
+	getName:function(){
+		console.log("tennis")
+	},
+	getBallSize:function(){
+		console.log("网球很小");
+	}
+};
+var SportsFactory = function(name){
+	switch (name) {
+		case 'NBA':
+			return new Basketball();
+		case 'WorldCup':
+			return new Football();
+		case 'FrenchOpen':
+			return new Tennis();
+		default:
+			return new Basketball();
+	}
+}
+
+var football = SportsFactory('WorldCup')
+console.log(football);
+console.log(football.intro);
+football.getMember();
+
+var tennis = SportsFactory("FrenchOpen")
+console.log(tennis);
+console.log(tennis.intro)
+tennis.getName();
+
+function Alert(){
+	this.name = "alert";
+	console.log("alert")
+}
+function Prompt(){
+	this.name = "prompt"
+	console.log("prompt")
+}
+
+var LoginAndSignFactory = function(name){
+	switch (name) {
+		case "alert":
+			return new Alert();
+		case "prompt":
+			return new Prompt()
+	}
+}
+
+var login = new LoginAndSignFactory("alert")
+console.log(login.name)
+
+var newlogin = new LoginAndSignFactory("prompt")
+console.log(newlogin.name)
+
+
+
+
+
+
+
+
+
+
+

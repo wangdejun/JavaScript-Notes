d6b797b2b462cf421715ec0b7848c34e8794d7d4
wangdejun <1074201799@qq.com>
2017-03-29 00:25:50 +0800

    master

--- 2_1prototype.js
+++ 2_1prototype.js
@@ -1,9 +1,10 @@
+//Book作为构造函数存在
 var Book = function(id, bookname, price){
 	this.id = id;
 	this.bookname = bookname;
 	this.price = price;
 }
-
+//函数原型链上添加方法
 Book.prototype= {
 	showBooKName : function(){return this.bookname;},
 	showId : function(){return this.id;},

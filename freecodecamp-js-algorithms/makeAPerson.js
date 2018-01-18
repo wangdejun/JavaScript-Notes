658a01d11e905b4e33732ca61992253543a69ee7
wangdejun <1074201799@qq.com>
2018-01-18 22:00:42 +0800

    function as constructor

---
+++ freecodecamp-js-algorithms/makeAPerson.js
@@ -0,0 +1,43 @@
+/**
+ * https://www.freecodecamp.org/challenges/make-a-person
+ * Make a Person
+ * @param {String} firstAndLast 
+ */
+
+var Person = function(firstAndLast) {
+    // Complete the method below and implement the others similarly
+    var fullArr = firstAndLast.split(' ');
+
+    var firstName = fullArr[0];
+    var lastName = fullArr[1];
+
+    this.getFirstName = function(){
+      return firstName;
+    };
+
+    this.getLastName = function(){
+      return lastName;
+    };
+
+    this.getFullName = function() {
+      return firstName + " "+lastName;
+    };
+
+    this.setFirstName = function(firstName_){
+        firstName = firstName_;
+    };
+
+    this.setLastName = function(lastName_){
+        lastName = lastName_;
+    };
+
+    this.setFullName = function(firstAndLast) {
+        fullName = firstAndLast;
+        firstName = firstAndLast.split(' ')[0];
+        lastName = firstAndLast.split(' ')[1];
+    };
+    return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
